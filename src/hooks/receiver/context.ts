c93a7ea44416e85d6e64e6089c355734db0a2641
achilles schmelzer <achilles@daopanel.com>
2022-10-21 10:48:07 -0700

    Add XMTP network to config

--- src/hooks/receiver/context.ts
+++ src/hooks/receiver/context.ts
@@ -1,14 +1,8 @@
 import { createContext } from 'react';
-import { Message, ListMessagesOptions, Conversation } from '@relaycc/xmtp-js';
 
 export interface ReceiverConfig {
-  api?: {
-    fetchMessages?: (
-      peerAddress: string,
-      opts?: Partial<ListMessagesOptions>
-    ) => Promise<Message[]>;
-    fetchConversations?: () => Promise<Conversation[]>;
-    startClient: (keys: Uint8Array) => Promise<boolean>;
+  xmtp?: {
+    network?: 'dev' | 'production';
   };
 }
 
